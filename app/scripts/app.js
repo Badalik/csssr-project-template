2604206caf70b09135b494e9491f097b1c9bb07b
Badalik <badaliki@yandex.ru>
2017-03-31 17:18:47 +0300

    feat(global) init

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,16 @@
 import svg4everybody from 'svg4everybody';
-import $ from 'jquery';
 
-$(() => {
-	svg4everybody();
-});
+const wrapper = document.getElementById('js-wrapper');
+const footer = document.getElementById('js-footer');
+const addPadding = function () {
+	if (wrapper) {
+		wrapper.style.paddingBottom = footer.offsetHeight + 'px';
+	}
+};
+
+svg4everybody();
+
+addPadding();
+window.addEventListener('load', addPadding);
+window.addEventListener('resize', addPadding);
+
