a08d5a68e3bf10b2ee8310b7fdd119da504a27f4
Badalik <badaliki@yandex.ru>
2017-06-06 18:20:54 +0300

    edit(global): edit for new rules

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,15 +1,6 @@
+import $ from 'jquery';
 import svg4everybody from 'svg4everybody';
 
-const wrapper = document.getElementById('js-wrapper');
-const footer = document.getElementById('js-footer');
-const addPadding = function () {
-	if (wrapper) {
-		wrapper.style.paddingBottom = footer.offsetHeight + 'px';
-	}
-};
-
-svg4everybody();
-
-addPadding();
-window.addEventListener('load', addPadding);
-window.addEventListener('resize', addPadding);
+$(() => {
+	svg4everybody();
+});
