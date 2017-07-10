3c48552a88cc94d42afeb80f4aef225fc62123f2
Badalik <badaliki@yandex.ru>
2017-07-10 15:51:34 +0300

    feat(tabs)

---
+++ app/blocks/tabs/tabs.js
@@ -0,0 +1,24 @@
+class Tabs {
+	constructor(wrapper) {
+		this.index = 0;
+		this.tab = wrapper.children();
+		this.length = this.tab.length;
+		this.tabActCl = 'tab_state_active';
+	}
+	skip(index) {
+		if (typeof index !== 'undefined') {
+			this.tab.eq(index).addClass(this.tabActCl).siblings().removeClass(this.tabActCl);
+			this.index = index;
+		} else if (this.index < this.length - 1) {
+			console.log('yes');
+			this.tab.eq(this.index).removeClass(this.tabActCl).next().addClass(this.tabActCl);
+			this.index++;
+		} else {
+			this.onTabsEnd();
+		}
+	}
+	onTabsEnd() {
+	}
+}
+
+export {Tabs};
