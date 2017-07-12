c7896347251bff4ba2f1bf5bb1fd4a9d7c7463f4
Badalik <badaliki@yandex.ru>
2017-07-12 14:06:46 +0300

    feat(tabs): add tabs to no minify js

--- app/resources/scripts/app.js
+++ app/resources/scripts/app.js
@@ -32,6 +32,29 @@ $(function () {
 			}
 		};
 
+		this.Tabs = function (wrapper) {
+			this.index = 0;
+			this.tab = wrapper.children();
+			this.length = this.tab.length;
+			this.tabActCl = 'tab_state_active';
+		};
+
+		this.Tabs.prototype = {
+			skip: function (index) {
+				if (typeof index !== 'undefined') {
+					this.tab.eq(index).addClass(this.tabActCl).siblings().removeClass(this.tabActCl);
+					this.index = index;
+				} else if (this.index < this.length - 1) {
+					this.tab.eq(this.index).removeClass(this.tabActCl).next().addClass(this.tabActCl);
+					this.index++;
+				} else {
+					this.onTabsEnd();
+				}
+			},
+			onTabsEnd: function () {
+			}
+		};
+
 		this.IntlTelInput = function (input) {
 			this.input = input;
 			this.value = 0;
