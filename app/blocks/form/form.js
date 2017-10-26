e4da09a7597dd2b3572496e848a93c2a373f782f
Badalik <badaliki@yandex.ru>
2017-10-26 12:00:13 +0300

    feat(form): add events and mbc record

--- app/blocks/form/form.js
+++ app/blocks/form/form.js
@@ -1,9 +1,11 @@
 import $ from 'jquery';
+import 'jquery-validation';
 import {IntlTelInput} from '../intl-tel-input/intl-tel-input';
 
 class Form {
 	constructor() {
-		this.wrapper = $('.js-form');
+		this.wrapper = $('.js-forms');
+		this.form = this.wrapper.find('.js-form');
 		this.flowForm = this.wrapper.find('.js-flow-form');
 		this.inputPhone = this.wrapper.find('.js-input_set_geo');
 		this.phone = new IntlTelInput(this.inputPhone);
@@ -25,10 +27,48 @@ class Form {
 				}
 			}
 		});
+
+		/*this.form.on('submit', function (e) {
+			e.preventDefault();
+			const $this = $(this);
+
+			form.formRecord($this);
+		});*/
+
+		/*this.skipTrig.on('click', function () {
+			const data = $(this).data();
+			form.skip();
+
+			if (!$.isEmptyObject(data)) {
+				for (const key in data) {
+					if ({}.hasOwnProperty.call(data, key)) {
+						mbc.session.set(key, data[key]);
+					}
+				}
+			}
+		});*/
+
+		/*this.check.on('change', function () {
+			const $parentForm = $(this).parents('.js-form');
+
+			$parentForm.serializeArray().forEach(function (item) {
+				mbc.session.set(item.name, item.value);
+			});
+
+			setTimeout(() => {
+			}, 600);
+		});*/
 	}
 	init() {
 		this.phone.init();
 		this.events();
+	}
+	formRecord(form) {
+		if (form.valid()) {
+			form.serializeArray().forEach(function (item) {
+				mbc.session.set(item.name, item.value);
+			});
+		}
 	}
 }
 
