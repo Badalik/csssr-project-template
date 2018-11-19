6495c30cc3fd64ad0aa5e3e3c1a8053935fb2e04
Badalik <badaliki@yandex.ru>
2018-11-19 10:46:34 +0300

    refactor(tabs): edit for no jQuery

--- app/blocks/tabs/tabs.js
+++ app/blocks/tabs/tabs.js
@@ -1,19 +1,23 @@
-import $ from 'jquery';
-
 class Tabs {
 	constructor(wrapper) {
 		this.index = 0;
-		this.wrapper = wrapper instanceof jQuery ? wrapper : $(wrapper);
-		this.tab = this.wrapper.children();
+		this.wrapper = typeof wrapper === 'string' ? document.getElementsByClassName(wrapper)[0] : wrapper;
+		this.tab = this.wrapper.children;
 		this.length = this.tab.length;
 		this.tabActCl = 'tab_state_active';
 	}
+
 	skip(index) {
 		if (typeof index !== 'undefined') {
-			this.tab.eq(index).addClass(this.tabActCl).siblings().removeClass(this.tabActCl);
+			for (let i = 0, l = this.tab.length; i < l; i++) {
+				this.tab[i].classList.remove(this.tabActCl);
+			}
+
+			this.tab[index].classList.add(this.tabActCl);
 			this.index = index;
 		} else if (this.index < this.length - 1) {
-			this.tab.eq(this.index).removeClass(this.tabActCl).next().addClass(this.tabActCl);
+			this.tab[this.index].classList.remove(this.tabActCl);
+			this.tab[this.index + 1].classList.add(this.tabActCl);
 			this.index++;
 		} else {
 			this.onTabsEnd();
@@ -27,10 +31,13 @@ class Tabs {
 			this.onTabsPreLast();
 		}
 	}
+
 	onTabsPreLast() {
 	}
+
 	onTabsLast() {
 	}
+
 	onTabsEnd() {
 	}
 }
--- app/resources/scripts/app.js
+++ app/resources/scripts/app.js
@@ -26,6 +26,50 @@ $(function () {
 			}
 		};
 
+		this.Tabs = function (wrapper) {
+			this.index = 0;
+			this.wrapper = typeof wrapper === 'string' ? document.getElementsByClassName(wrapper)[0] : wrapper;
+			this.tab = this.wrapper.children;
+			this.length = this.tab.length;
+			this.tabActCl = 'tab_state_active';
+		};
+
+		this.Tabs.prototype = {
+			skip: function (index) {
+				if (typeof index !== 'undefined') {
+					for (let i = 0, l = this.tab.length; i < l; i++) {
+						this.tab[i].classList.remove(this.tabActCl);
+					}
+
+					this.tab[index].classList.add(this.tabActCl);
+					this.index = index;
+				} else if (this.index < this.length - 1) {
+					this.tab[this.index].classList.remove(this.tabActCl);
+					this.tab[this.index + 1].classList.add(this.tabActCl);
+					this.index++;
+				} else {
+					this.onTabsEnd();
+				}
+
+				if (this.index === this.length - 1) {
+					this.onTabsLast();
+				}
+
+				if (this.index + 1 === this.length - 1) {
+					this.onTabsPreLast();
+				}
+			},
+
+			onTabsPreLast: function () {
+			},
+
+			onTabsLast: function () {
+			},
+
+			onTabsEnd: function () {
+			}
+		};
+
 		this.Form = function () {
 			this.form = $('.js-form');
 			this.inputPhone = this.form.find('.js-input-phone');
